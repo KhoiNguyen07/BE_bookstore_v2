99723a87c20ca5a80beb380368c35173bd3e63e6
Nguyen khac Trieu <kt@gmail.com>
2025-10-23 16:37:16 +0700

    new

--- src/main/java/com/devsoga/BookStore_V2/repositories/ProductRepository.java
+++ src/main/java/com/devsoga/BookStore_V2/repositories/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.devsoga.BookStore_V2.repositories;
 
-import com.devsoga.BookStore_V2.enties.ProductEntity;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.devsoga.BookStore_V2.enties.ProductEntity;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
